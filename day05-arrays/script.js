d7654b5f6a1042c2e736b17891186d4eb6052fad
JustinWong98 <justinwong8991@gmail.com>
2022-02-13 09:56:50 +0800

    up to day 7 done

--- day05-arrays/script.js
+++ day05-arrays/script.js
@@ -19,12 +19,16 @@ var temperatures = [37.1, 36.3, 37.3, 36.7, 36.9, 36.8, 37.0]
 // The maximum size of an array (how many pieces of seperate data it can store) is around 2^32-1 = 4.29 billion elements. Obviously, we won't ever want to get to that number since it will lag out our computer. https://stackoverflow.com/questions/6154989/maximum-size-of-an-array-in-javascript
 
 // What should we store inside an array?
-// BAD EXAMPLE
-var myValues = [23, 'rocks', 0.345, 'awesome']; // This is technically correct (javascript allows different data types to exist in the same array or data collection)
-// But just like the naming of variables - what kind of data we store and how we name it is of upmost importance. Our above statement doesn't seem to make much sense.
-// Arrays Only Store 1 "Kind" of Data at a Time - given that we only have one name we can give to an array as a variable, the data that falls in it must be all related through what we have named the array as.
+var letters = ['a', 'b', 'c'];
+// Q1: Can I only store 1 data type at a time?
+// Q2: How do I change array to be ['a', 'b', 'c', 'd']?
 
-// GOOD EXAMPLES
+// Q3: How we would change an element given its index?
+// Example: change the below array to be correct. 'e' should be 'd'
+var letters = ['a', 'b', 'c', 'e']
+// ANS: letters[3] = 'd' or letters[letters.length - 1] = 'd'
+
+
 var daysOfTheWeek = [
   'Monday',
   'Tuesday',
@@ -84,20 +88,150 @@ daysOfTheWeek[1]; // gives us Tuesday.
 // We can get the length of an array through arrayName.length - this is a method which all arrays have access to.
 daysOfTheWeek.length // gives us 7
 
-// ASK A STUDENT: Given what we know above, ask a student how we can always access the last element of an array no matter what the size is?
+// Q4: Given what we know above, ask a student how we can always access the last element of an array no matter what the size is?
 // ANS: arrayName[arrayName.length - 1], you can use daysOfTheWeek as an example and show it in console.
 
-// ASK A STUDENT: What happens if we try to access an index that doesn't exist?
+// Q5: What happens if we try to access an index that doesn't exist?
 // ANS: undefined. Show it in console.
 
-// ASK A STUDENT: How we would change an element given its index?
-// Example: change the below array to be correct. 'e' should be 'd'
-var letters = ['a', 'b', 'c', 'e']
-// ANS: letters[3] = 'd' or letters[letters.length - 1] = 'd'
-
-// BONUS: Ask why we would normally use letters[letters.length - 1] over letters[3] to access the last index??
+// Q6: Ask why we would normally use letters[letters.length - 1] over letters[3] to access the last index??
 // ANS: We may not always be able to know the length of letters or any array with dynamic length at the time we execute the code. The length of the array minus 1 is the most consistent way. What if the letters array was longer or shorter?
 
 // Adding and removing elements
 // We can append or remove elements from an array using methods known as pop or push. We access those methods similar to how we access length.
 // Demonstrate using letters array, popping and pushing. Show the students how we only are affecting the rightmost side of the array and not the leftmost side.
+
+// Q7: What does the below function do?
+var guesses = [];
+var main = function (input) {
+  guesses.push(input);
+  var randomDiceNumber = rollDice();
+  var myOutputValue = 'You lose. Your guesses: ' + guesses;
+  if (randomDiceNumber == input) {
+    myOutputValue = 'You win. Your guesses: ' + guesses;
+  }
+  return myOutputValue;
+};
+
+// ANS: Pseudo coded code below
+var guesses = [];
+var main = function (input) {
+  // Add the user's guess to the guesses array.
+  guesses.push(input);
+  // Generate a random dice number.
+  var randomDiceNumber = rollDice();
+  // Initialise output to communicate loss.
+  // Output the record of all guesses regardless of loss or win.
+  var myOutputValue = 'You lose. Your guesses: ' + guesses;
+  // If the guess matches the dice roll, change output to communicate win.
+  if (randomDiceNumber == input) {
+    myOutputValue = 'You win. Your guesses: ' + guesses;
+  }
+  // Return output value.
+  return myOutputValue;
+};
+
+// Q8: What does this function do? What kind of shape do we expect with a certain input (e.g 4)?
+var main = function (input) {
+  var myOutputValue = '';
+  var rowCounter = 0;
+  while (rowCounter < input) {
+    var columnCounter = 0;
+    while (columnCounter < input) {
+      myOutputValue = myOutputValue + 'x';
+      columnCounter = columnCounter + 1;
+    }
+    myOutputValue = myOutputValue + '<br>';
+    rowCounter = rowCounter + 1;
+  }
+  return myOutputValue;
+};
+
+// ANS: Pseudo coded code below
+var main = function (input) {
+  var myOutputValue = '';
+  // Initialise the outer counter, rowCounter
+  var rowCounter = 0;
+  while (rowCounter < input) {
+    // Inside the outer loop, initialise the inner counter, columnCounter
+    var columnCounter = 0;
+    // Every time the outer loop runs, the inner loop runs repeatedly until
+    // the inner loop condition is met.
+    while (columnCounter < input) {
+      // Each time the inner loop runs, it adds "x" to output
+      myOutputValue = myOutputValue + 'x';
+      columnCounter = columnCounter + 1;
+    }
+    // At the end of each outer loop, add a <br> tag to begin a new row
+    myOutputValue = myOutputValue + '<br>';
+    rowCounter = rowCounter + 1;
+  }
+  // After the outer loop has run to completion, return the output compiled
+  // by the above loops.
+  return myOutputValue;
+};
+
+
+// Using the length of an array, we can simply loop over the elements of an array.
+var letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+var main = function (input) {
+  var myOutputValue = '';
+  var counter = 0;
+
+  while (counter < letters.length) {
+    console.log(letters[counter]);
+    myOutputValue = myOutputValue + 'yes';
+    counter = counter + 1;
+  }
+  return myOutputValue;
+};
+
+// Q9: Explain what this function does
+var names = [];
+var main = function (input) {
+  var found = false;
+  var index = 0;
+  while (index < names.length) {
+    var currentName = names[index];
+    if (currentName == input) {
+      found = true;
+    }
+    index = index + 1;
+  }
+
+  console.log(found)
+  if (!found) {
+    names.push(input);
+  }
+
+  var myOutputValue = 'All names: ' + names;
+  return myOutputValue;
+};
+
+// Q10: Pseudocode version below
+var main = function (input) {
+  // Set a boolean value found to a default value of false
+  var found = false;
+  
+  // Loop over the names array, and set found to true if the input name already
+  // exists in the names array
+  var index = 0;
+  while (index < names.length) {
+    var currentName = names[index];
+    if (currentName == input) {
+      found = true;
+    }
+    index = index + 1;
+  }
+
+  console.log(found)
+  // If no duplicate name was found, add the input name to the names array 
+  if (!found) {
+    names.push(input);
+  }
+
+  // Return the full array of names
+  var myOutputValue = 'All names: ' + names;
+  return myOutputValue;
+};
--- day07-objects/script.js
+++ day07-objects/script.js
@@ -4,7 +4,7 @@
 // Example: Cards of a deck.
 var cardDeck = ['nine of hearts', 'ten of hearts', 'jack of hearts'];
 
-// How would we obtain the suit of each card in this array? (students are not expected to know how to do it)
+// Q1: How would we obtain the suit of each card in this array? (students are not expected to know how to do it)
 // ANS: We would have to access the element through its index, and then attempt to try and find the name of the suit by analysing the string.
 // BONUS: You can talk about splitting here. Below is how we would do it. If you do decide to show how it is done, stress to the students that the point is not to understand splitting arrays - it is just a demonstration to show how arrays are ineffective here for our problem.
 var firstCard = cardDeck[0];
@@ -26,23 +26,393 @@ var playingCard = {
   // Store an additional "name" attribute to correctly name face cards and aces.
   name: 'jack',
 };
-// ASK A STUDENT: How would they access the suit of this card now?
+// Q2: How would they access the suit of this card now?
 // ANS: playingCard.suit
 
 // The students should understand that for objects, we tie a value to a key (e.g value 'heart' to key 'suit'). We access the value through the key pointing to it.
 
-// ASK A STUDENT: The above solution is great, but there seems to be a problem unaddressed in using an object. What is it? What would you propose to fix that issue?
+// Q3: The above solution is great, but there seems to be a problem unaddressed in using an object so far compared to an array. What is it? What would you propose to fix that issue?
 // ANS: The object can only seem to store one card as opposed to an array which can store multiple cards. We can use an array of objects to simulate a card deck.
-var cardDeck = [
+var deck = [
   {
+    name: 'ace',
+    suit: 'hearts',
     rank: 1,
-    suit: 'heart',
-    name: 'one',
   },
   {
+    name: '2',
+    suit: 'hearts',
     rank: 2,
-    suit: 'heart',
-    name: 'two',
   },
-  // ...
+  {
+    name: '3',
+    suit: 'hearts',
+    rank: 3,
+  },
+  {
+    name: '4',
+    suit: 'hearts',
+    rank: 4,
+  },
+  {
+    name: '5',
+    suit: 'hearts',
+    rank: 5,
+  },
+  {
+    name: '6',
+    suit: 'hearts',
+    rank: 6,
+  },
+  {
+    name: '7',
+    suit: 'hearts',
+    rank: 7,
+  },
+  {
+    name: '8',
+    suit: 'hearts',
+    rank: 8,
+  },
+  {
+    name: '9',
+    suit: 'hearts',
+    rank: 9,
+  },
+  {
+    name: '10',
+    suit: 'hearts',
+    rank: 10,
+  },
+  {
+    name: 'jack',
+    suit: 'hearts',
+    rank: 11,
+  },
+  {
+    name: 'queen',
+    suit: 'hearts',
+    rank: 12,
+  },
+  {
+    name: 'king',
+    suit: 'hearts',
+    rank: 13,
+  },
+  {
+    name: 'ace',
+    suit: 'diamonds',
+    rank: 1,
+  },
+  {
+    name: '2',
+    suit: 'diamonds',
+    rank: 2,
+  },
+  {
+    name: '3',
+    suit: 'diamonds',
+    rank: 3,
+  },
+  {
+    name: '4',
+    suit: 'diamonds',
+    rank: 4,
+  },
+  {
+    name: '5',
+    suit: 'diamonds',
+    rank: 5,
+  },
+  {
+    name: '6',
+    suit: 'diamonds',
+    rank: 6,
+  },
+  {
+    name: '7',
+    suit: 'diamonds',
+    rank: 7,
+  },
+  {
+    name: '8',
+    suit: 'diamonds',
+    rank: 8,
+  },
+  {
+    name: '9',
+    suit: 'diamonds',
+    rank: 9,
+  },
+  {
+    name: '10',
+    suit: 'diamonds',
+    rank: 10,
+  },
+  {
+    name: 'jack',
+    suit: 'diamonds',
+    rank: 11,
+  },
+  {
+    name: 'queen',
+    suit: 'diamonds',
+    rank: 12,
+  },
+  {
+    name: 'king',
+    suit: 'diamonds',
+    rank: 13,
+  },
+  {
+    name: 'ace',
+    suit: 'clubs',
+    rank: 1,
+  },
+  {
+    name: '2',
+    suit: 'clubs',
+    rank: 2,
+  },
+  {
+    name: '3',
+    suit: 'clubs',
+    rank: 3,
+  },
+  {
+    name: '4',
+    suit: 'clubs',
+    rank: 4,
+  },
+  {
+    name: '5',
+    suit: 'clubs',
+    rank: 5,
+  },
+  {
+    name: '6',
+    suit: 'clubs',
+    rank: 6,
+  },
+  {
+    name: '7',
+    suit: 'clubs',
+    rank: 7,
+  },
+  {
+    name: '8',
+    suit: 'clubs',
+    rank: 8,
+  },
+  {
+    name: '9',
+    suit: 'clubs',
+    rank: 9,
+  },
+  {
+    name: '10',
+    suit: 'clubs',
+    rank: 10,
+  },
+  {
+    name: 'jack',
+    suit: 'clubs',
+    rank: 11,
+  },
+  {
+    name: 'queen',
+    suit: 'clubs',
+    rank: 12,
+  },
+  {
+    name: 'king',
+    suit: 'clubs',
+    rank: 13,
+  },
+  {
+    name: 'ace',
+    suit: 'spades',
+    rank: 1,
+  },
+  {
+    name: '2',
+    suit: 'spades',
+    rank: 2,
+  },
+  {
+    name: '3',
+    suit: 'spades',
+    rank: 3,
+  },
+  {
+    name: '4',
+    suit: 'spades',
+    rank: 4,
+  },
+  {
+    name: '5',
+    suit: 'spades',
+    rank: 5,
+  },
+  {
+    name: '6',
+    suit: 'spades',
+    rank: 6,
+  },
+  {
+    name: '7',
+    suit: 'spades',
+    rank: 7,
+  },
+  {
+    name: '8',
+    suit: 'spades',
+    rank: 8,
+  },
+  {
+    name: '9',
+    suit: 'spades',
+    rank: 9,
+  },
+  {
+    name: '10',
+    suit: 'spades',
+    rank: 10,
+  },
+  {
+    name: 'jack',
+    suit: 'spades',
+    rank: 11,
+  },
+  {
+    name: 'queen',
+    suit: 'spades',
+    rank: 12,
+  },
+  {
+    name: 'king',
+    suit: 'spades',
+    rank: 13,
+  },
 ];
+
+// Q4: What's the problem with this? What would be a more efficient way of generating a card deck?
+// ANS: The code is more than 200 lines long. We can use a loop to generate the deck.
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = 'ace';
+      } else if (cardName == 11) {
+        cardName = 'jack';
+      } else if (cardName == 12) {
+        cardName = 'queen';
+      } else if (cardName == 13) {
+        cardName = 'king';
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
+// Q5: Get a student to walk through the code for this high card game
+
+// // Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+// Shuffle the deck and save it in a new variable shuffledDeck
+// to communicate that we have shuffled the deck.
+var shuffledDeck = shuffleCards(deck);
+
+var main = function (input) {
+  // Draw 2 cards from the top of the deck
+  var computerCard = shuffledDeck.pop();
+  var playerCard = shuffledDeck.pop();
+
+  // Construct an output string to communicate which cards were drawn
+  var myOutputValue =
+    'Computer had ' +
+    computerCard.name +
+    ' of ' +
+    computerCard.suit +
+    '. Player had ' +
+    playerCard.name +
+    ' of ' +
+    playerCard.suit +
+    '. ';
+
+  // Compare computer and player cards by rank attribute
+  // If computer card rank is greater than player card rank, computer wins
+  if (computerCard.rank > playerCard.rank) {
+    // Add conditional-dependent text to the output string
+    myOutputValue = myOutputValue + 'Computer wins.';
+    // Else if computer card rank is less than player card rank, player wins
+  } else if (computerCard.rank < playerCard.rank) {
+    myOutputValue = myOutputValue + 'Player wins!';
+    // Otherwise (i.e. ranks are equal), it's a tie
+  } else {
+    myOutputValue = myOutputValue + "It's a tie.";
+  }
+
+  // Return the fully-constructed output string
+  return myOutputValue;
+};
